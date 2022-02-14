ca437641180098c65fbeff85d07ddbf05a8fc39c
Lucas Fernandes <lucasfernandes_222@hotmail.com>
2022-02-14 07:09:59 -0300

    first commit

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RifugoApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RifugoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly UsuarioContext context;
+
+        public UsuarioController(UsuarioContext context)
+        {
+            this.context = context;
+        }
+        // GET: api/<UsuarioController>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try {
+                return Ok(context.usuarios.ToList());
+            } catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // GET api/<UsuarioController>/5
+        [HttpGet("{id}", Name ="GetUsuario")]
+        public ActionResult Get(int id)
+        {
+            try
+            {
+                var usuario = context.usuarios.FirstOrDefault(u => u.Id == id);
+                return Ok(usuario);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // POST api/<UsuarioController>
+        [HttpPost]
+        public ActionResult Post([FromBody] Usuario usuario)
+        {
+            try
+            {
+                context.usuarios.Add(usuario);
+                context.SaveChanges();
+                return CreatedAtRoute("GetUsuario", new { id = usuario.Id }, usuario);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // PUT api/<UsuarioController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Usuario usuario)
+        {
+            try
+            {
+                if(usuario.Id == id)
+                {
+                    context.Entry(usuario).State = EntityState.Modified;
+                    context.SaveChanges();
+                    return CreatedAtRoute("GetUsuario", new { id = usuario.Id }, usuario);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE api/<UsuarioController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var usuario = context.usuarios.FirstOrDefault(g => g.Id == id);
+                if (usuario != null)
+                {
+                    context.usuarios.Remove(usuario);
+                    context.SaveChanges();
+                    return Ok(id);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RifugoApi.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+       
+        public string Nome { get; set; }
+
+       
+        public string Telefone { get; set; }
+
+       
+        public string Email { get; set; }
+    }
+}
